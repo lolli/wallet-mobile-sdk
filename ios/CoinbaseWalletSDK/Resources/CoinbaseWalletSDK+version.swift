86507d3e6725b6506cb1e2e3ab7c8092be45500e
vincentvillalta <vincentvillalta@me.com>
2024-05-31 14:41:53 +0200

    remove pragma macro limitation

--- ios/CoinbaseWalletSDK/Resources/CoinbaseWalletSDK+version.swift
+++ ios/CoinbaseWalletSDK/Resources/CoinbaseWalletSDK+version.swift
@@ -11,9 +11,9 @@ extension CoinbaseWalletSDK {
     // CFBundleShortVersionString doesn't exist if the SDK is built as a static library.
     static private(set) var version = "1.1.0"
     
-    #if CROSS_PLATFORM
+    // #if CROSS_PLATFORM
     static public func appendVersionTag(_ tag: String) {
         self.version += "/\(tag)"
     }
-    #endif
+    // #endif
 }
